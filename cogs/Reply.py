23706a110fd896e84030a2413aaffc5417dff400
BuildTools <unconfigured@null.spigotmc.org>
2020-07-29 14:20:09 +1000

    Large clean up of long and deeply nested if statement chain in Reply.py by creating the responses.py module to handle the different cases

--- cogs/Reply.py
+++ cogs/Reply.py
@@ -6,26 +6,35 @@ from discord.ext import commands
 from datetime import datetime
 
 import database_helper as db
-
+import cogs.reply_helper.responses as r
 """
 Module for listening to all messages and replying accordingly
 """
 
 BOT_ID = 729244809021358141
+KURO_ID = 109590357276098560
 
 class Reply(commands.Cog):
+
 
     def __init__(self, client):
         self.client = client
 
-    # # Actual command definition
-    # @commands.command()
-    # async def test(self, ctx):
-    #     """Function made to be overwritten for testing purposes"""
-    #     await ctx.send("Pong!")
 
-    def message_contains(self, word, currMessage):
-        return re.search(f".*{word}.*", currMessage, re.IGNORECASE)
+    async def handle_pings(self, ctx):
+        # Check if people have pinged kuro
+        for user in ctx.mentions:
+            if user.id == BOT_ID or user.id == KURO_ID: 
+                db.increase_stat(ctx.author.id, "pinged_kuro")
+                await ctx.channel.send("<:yesdaddy:719927492890198087>")
+
+
+    # Records my direct messages to Kuro Bot so I can just copy and paste chunks of messages
+    def debugging_logs(self, ctx):
+        with open("log.txt", 'a+') as file:
+            timestamp = datetime.now().strftime("[%d/%m/%Y][%H:%M:%S]")
+            file.write(f"{timestamp} {ctx.author}: {ctx.content}\n")
+
 
     # ctx.guild/message/author/send()
     # Listens to the chat and replies when it hears certain lines
@@ -35,58 +44,19 @@ class Reply(commands.Cog):
         currMessage = ctx.content
         did_talk = False  # Flag to check if the author talked with kuro bot
 
-        # TODO: refactor this whole if statement chain, probably functionalise this to avoid yandere simulator memery
         if ctx.author.id != BOT_ID:
             if ctx.guild is None:
+                self.debugging_logs(ctx)
                 did_talk = True
-                with open("log.txt", 'a+') as file:
-                    timestamp = datetime.now().strftime("[%d/%m/%Y][%H:%M:%S]")
-                    print(ctx.content)
-                    file.write(f"{timestamp} {ctx.author}: {ctx.content}\n")
 
-            if self.message_contains("kuro", currMessage):
-                did_talk = True
-                dialogue = random.choice(["yes", "hmm", "indeed", "i see", "wait what", "ooooookaaaaaaayyyy", "mmhmm", "ok", "correct", "uuuuuuh"])
-                await ctx.channel.send(dialogue)
-
-            elif self.message_contains("kuwo", currMessage):
-                db.increase_stat(ctx.author.id, "owo")
-                did_talk = True
-                dialogue = random.choice(["yes :3", "hmm owo", "indeed >\\<", "i see uwu", "ooookayyy owo", "wwwait what", "uuuuuuuh.."])
-                await ctx.channel.send(dialogue)
-
-            elif self.message_contains("aiya|aya", currMessage):
-                did_talk = True
-                await ctx.channel.send("ayayayayayaya")
-            
-            if self.message_contains(">V|>:V", currMessage):
-                did_talk = True
-                await ctx.add_reaction(emoji="<:bertV:732834829040877640>")
-
-            if self.message_contains("is gay|daddy", currMessage):
-                did_talk = True
-                await ctx.channel.send("<:yesdaddy:719927492890198087>")
-
-            for user in ctx.mentions:
-                print(f"user(s) mentioned were {user}")
-                
-                if user.id == 729244809021358141 or user.id == 109590357276098560: 
-                    db.increase_stat(ctx.author.id, "pinged_kuro")
-                    await ctx.channel.send("<:yesdaddy:719927492890198087>")
+            did_talk = await r.match_message(ctx, currMessage)
+            await self.handle_pings(ctx)
 
             if did_talk:
                 db.increase_stat(ctx.author.id, "times_talked")
-    
-        msgs = []
-        async for message in ctx.channel.history(limit=3):
-            msgs.append(message.content)
-
-        last_msg = msgs[1]
-        second_last = msgs[0]
-        if last_msg == "wtf i'm scared" and second_last == "hi scared":
-            await ctx.channel.send("eeeeeeeeeeeeeeeeeeeeee")
+        
+        await r.check_prev_msg(ctx)
 
 
 def setup(client):
     client.add_cog(Reply(client))
-
---
+++ cogs/reply_helper/responses.py
@@ -0,0 +1,84 @@
+import database_helper as db
+import re
+import random
+
+"""
+Helper file to handle responses by Kuro Bot
+"""
+
+async def kuro(ctx):
+    dialogue = random.choice(["yes", "hmm", "indeed", "i see", "wait what", "ooooookaaaaaaayyyy", "mmhmm", "ok", "correct", "uuuuuuh"])
+    await ctx.channel.send(dialogue)
+    return True
+
+async def kuwo(ctx):
+    db.increase_stat(ctx.author.id, "owo")
+    dialogue = random.choice(["yes :3", "hmm owo", "indeed >\\<", "i see uwu", "ooookayyy owo", "wwwait what", "uuuuuuuh.."])
+    await ctx.channel.send(dialogue)
+    return True
+
+async def aiya(ctx):
+    await ctx.channel.send("ayayayayayaya")
+    return True
+
+async def grr(ctx):
+    await ctx.add_reaction(emoji="<:bertV:732834829040877640>")
+    return True
+
+async def daddy(ctx):
+    await ctx.channel.send("<:yesdaddy:719927492890198087>")
+    return True
+
+def message_contains(word, currMessage):
+    return re.search(f".*{word}.*", currMessage, re.IGNORECASE)
+
+async def match_message(ctx, currMessage):
+    # Loop through array to simulate if/else statements
+    message_to_match = [
+        ["kuro", "kuwo", "aiya|aya"],   # Nested list elements are mutually exclusive to each other (basically an if else)
+        ">V|>:V",                       # Unnested items can be executed alongside other items (pure if statement)
+        "is gay|daddy",
+        ]
+
+    get_response = {
+        "kuro":         kuro,
+        "kuwo":         kuwo,
+        "aiya|aya":     aiya,
+        ">V|>:V":       grr,
+        "is gay|daddy": daddy,
+    }
+    did_talk = False
+    # Find the function matching the message that we want to match (the mask)
+    for mask in message_to_match:
+        if isinstance(mask, list):
+            for m in mask:
+                if message_contains(m, currMessage):
+                    did_talk = await get_response[m](ctx)
+                    break
+        else:
+            if message_contains(mask, currMessage):
+                did_talk = await get_response[mask](ctx)
+
+    return did_talk
+
+
+# Handle cases where we want to match multiple messages
+# I don't think matching more than just the previous message is necessary (since I don't think we'd use it ever)
+#   also we can chain checks for the previous message if we did want to match more than just the previous message
+async def check_prev_msg(ctx):
+    msgs = []
+    async for message in ctx.channel.history(limit=3):
+        msgs.append(message.content)
+
+    last_msg = msgs[1]
+    second_last = msgs[0]
+
+    messages_to_match = {
+        ("wtf i'm scared", "hi scared"): "eeeeeeeeeeeeeeeeeeeeee"
+    }
+
+    try:
+        resp = messages_to_match[(last_msg, second_last)]
+        await ctx.channel.send(resp)
+    except KeyError:
+        pass
